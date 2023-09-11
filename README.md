b4dc4f38f7fc05d593cea74e9c033f4dbe9c092a
Cicilini <guilherme.cicilini@hotmail.com>
2023-09-10 22:35:35 -0300

    1.0.3 validação-do-input

---
+++ README.md
@@ -0,0 +1,18 @@
+ <h1>:money_with_wings: Conversor de Moeda </h1>
+
+Este é um simples conversor de moeda que permite aos usuários converter valores de uma moeda para outra.
+
+Obs: Neste momento estou trabalhando com taxas de cambio aproximadas (futuramente irei integrar com uma API)
+
+![Screenshot](screenshot.png)
+
+## Funcionalidades
+
+- Conversão de moeda em tempo real.
+- Suporte a várias moedas, incluindo Real (BRL), Dólar (USD), Euro (EUR), Libra Esterlina (GBP), Iene Japonês (JPY).
+- Interface de usuário amigável e fácil de usar.
+- Validação de entrada para garantir que os valores inseridos sejam válidos.
+- Exibição de resultados formatados com precisão.
+
+## <h2> Link </h2>
+   git clone https://github.com/seu-usuario/conversor-de-moeda.git
---
+++ script.js
@@ -0,0 +1,63 @@
+const real = 4.95;
+const euro = 0.91;
+const libra = 0.78;
+const iene = 145.46;
+const dolar = 1;
+
+const select = document.querySelector('.coin1');
+const input = document.querySelector('.input-number');
+const select2 = document.querySelector('.coin2')
+const result = document.querySelector('.result');
+
+let vazio = input.value;
+
+
+
+
+function validarcampo() {
+    if (input.value <= 0 && input.value != vazio) {
+        alert("Preencha com valores positivos!")
+        return true
+    } else {
+        conversao()
+    }
+}
+
+function conversao() {
+    const valorInput = parseFloat(input.value);
+    const moedaDe = select.value;
+    const moedaPara = select2.value;
+
+    let valor = 0;
+
+    if (input.value == vazio) {
+        alert("Preencha um valor válido maior que zero.");
+    } else {
+        if (moedaDe === "real") {
+            valor = valorInput / real; // Inverter a conversão aqui
+        } else if (moedaDe === "euro") {
+            valor = valorInput / euro;
+        } else if (moedaDe === "libra") {
+            valor = valorInput / libra;
+        } else if (moedaDe === "iene") {
+            valor = valorInput / iene;
+        } else if (moedaDe === "dolar") {
+            valor = valorInput / dolar;
+        }
+
+        let resultado = 0;
+        if (moedaPara === "real") {
+            resultado = "R$ " + (valor * real).toFixed(2); // Multiplicar a conversão aqui
+        } else if (moedaPara === "dolar") {
+            resultado = "US$ " + (valor * dolar).toFixed(2);
+        } else if (moedaPara === "euro") {
+            resultado = "€ " + (valor * euro).toFixed(2);
+        } else if (moedaPara === "libra") {
+            resultado = "£ " + (valor * libra).toFixed(2);
+        } else if (moedaPara === "iene") {
+            resultado = "¥ " + (valor * iene).toFixed(4);
+        }
+        result.innerHTML = resultado;
+    }
+}
+
